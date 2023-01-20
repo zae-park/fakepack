414ff8ccfd4676e9465b7fe11d0d1a6c20a08b11
zae_park <zae_park@huinno.com>
2023-01-20 17:47:21 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,26 @@
 import sys
-import argparser
-from setuptools import setup, Expension
+import argparse
+from setuptools import setup, Extension
 
-with open("README.md", "r", encoding="utf-8") as me:
-    long_description = me.read()
 
-argparser.add_argument('-n', '--name', description = 'Package name.')
-argparser.add_argument('-l', '--link', description = 'Package link - git.')
+parser = argparse.ArgumentParser(
+    prog='ProgramName',
+    description='What the program does',
+    epilog='Text at the bottom of help')
 
-setuptools.setup(
-    name="example-pkg-scdev-pd-columns",
-    version="0.0.1",
-    author="Schooldevops",
-    author_email="schooldevops@gmail.com",
-    description="schooldevops sample lib",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/schooldevops/python-tutorials",
-    project_urls={
-        "Bug Tracker": "https://github.com/schooldevops/python-tutorials/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+parser.add_argument('--name', type=str)
+parser.add_argument('--link', type=str)
+
+args = parser.parse_args()
+
+name = args.name
+url = args.link
+
+setup(
+    name=name,
+    version="0.0.0",
+    author="anonymous",
+    description=f"Fake package of {name}.",
+    dependency_links=[url],
+    python_requires=">=3.8"
 )
