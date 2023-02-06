81b305eda6463c0437725b11362283833c215fc6
zae_park <zae_park@huinno.com>
2023-02-06 17:52:15 +0900

    add some cli function.

--- fakepack/fakepack.py
+++ fakepack/fakepack.py
@@ -2,8 +2,23 @@ import os
 import click
 
 
-@click.argument('name')
-@click.argument('url')
+@click.command()
+@click.argument('name', nargs=1)
+@click.argument('url', nargs=1)
 def fake(name, url):
-    os.system(f'poetry add {url}')
-    print('zae-park')
+    print(f'{name} - {url}')
+
+
+@click.command()
+@click.argument('src', nargs=-1)
+@click.argument('dst', nargs=1)
+def copy(src, dst):
+    """Move file SRC to DST."""
+    for fn in src:
+        click.echo('move %s to folder %s' % (fn, dst))
+
+
+@click.command()
+def dummy():
+    """Move file SRC to DST."""
+    print('dummy')
