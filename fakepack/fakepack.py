0aa69c595ae846349e0767bdf28eb854648f1b0b
zae_park <zae_park@huinno.com>
2023-02-07 18:19:14 +0900

    add initial sub-command and description

--- fakepack/fakepack.py
+++ fakepack/fakepack.py
@@ -2,11 +2,46 @@ import os
 import click
 
 
+
+
 @click.command()
-@click.argument('name', nargs=1)
-@click.argument('url', nargs=1)
-def fake(name, url):
+@click.argument('sub_command', nargs=1)
+@click.argument('args', nargs=-1)
+def fake(sub_command, args):
+    """
+    These are common Fakepack commands used in various situations:
+
+    Authorization
+        init        Resistrate author's information (or token).
+    
+    Manage dependency
+        add         Resistrate author's information (or token).
+
+    - init
+    - add
+    - publish
+    """
+    assert sub_command in ['init', 'add', 'publish'], f'The command {sub_command} does not exist.'
+    
     print(f'{name} - {url}')
+
+
+class Tokenizer:
+    def __init__(self) -> None:
+        pass
+
+    def p(a):
+        print(a)
+
+
+class Manager:
+    def __init__(self) -> None:
+        pass
+
+    def m(a):
+        print(a)
+
+
 
 
 @click.command()
